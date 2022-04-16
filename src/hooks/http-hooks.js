7cc596ee9215911db9efc9d141da74626504666d
Emilio <toschi17@yahoo.i>
2022-04-16 15:21:21 +0200

    Utilizzo server su Heroku

--- src/hooks/http-hooks.js
+++ src/hooks/http-hooks.js
@@ -11,8 +11,8 @@ export const useHttpClient = () => {
 
 	const currentAppName = 'my-work';
 
-	const SRV = `http://${SRV_name}:${SRV_port}/`;
-	// const SRV = `https://my-work-testing.herokuapp.com/`;
+	// const SRV = `http://${SRV_name}:${SRV_port}/`;
+	const SRV = `https://my-work-testing.herokuapp.com/`;
 
 	const sendRequest = useCallback(
 		async (url, method = 'GET', body = undefined, headers = {}) => {
